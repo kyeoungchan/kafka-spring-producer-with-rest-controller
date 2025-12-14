24ec0c6d61c9e571b0ecb9554f6ab518b840333a
wkc1002 <wkc1002@naver.com>
2025-12-14 15:57:12 +0900

    init

---
+++ src/main/java/com/example/kafkaspringproducerwithrestcontroller/KafkaSpringProducerWithRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringproducerwithrestcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringProducerWithRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringProducerWithRestControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaspringproducerwithrestcontroller/KafkaSpringProducerWithRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspringproducerwithrestcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringProducerWithRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
